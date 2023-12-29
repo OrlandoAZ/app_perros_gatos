b517a0ec4869a53bc22464182bf2010f326cc2d2
OrlandoAZ <46329605+OrlandoAZ@users.noreply.github.com>
2023-12-29 15:42:23 -0500

    Add files via upload

---
+++ streamlitfinal.py
@@ -0,0 +1,80 @@
+# pip install streamlit
+## pip install pillow
+#pip install --upgrade keras
+#pip install --upgrade tensorflow
+#pip install opencv-python
+#pip install matplotlib
+#pip install scikit-image
+
+import streamlit as st
+import numpy as np
+import os
+from keras.models import load_model
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+from skimage import io
+
+# Cargar el modelo de Keras
+model = load_model('keras_model.h5')
+
+# Cargar las etiquetas
+class_names = open("labels.txt", "r").readlines()
+
+# Crear el directorio temporal si no existe
+temp_dir = "temp"
+os.makedirs(temp_dir, exist_ok=True)
+
+# Función de preprocesamiento y clasificación de la imagen
+def clasificar_imagen(imagen_path):
+    img_array = io.imread(imagen_path) / 255.0
+    img_resized = ImageOps.fit(Image.fromarray((img_array * 255).astype(np.uint8)), (224, 224), Image.Resampling.LANCZOS)
+    img_array_resized = np.asarray(img_resized)
+    normalized_image_array = (img_array_resized.astype(np.float32) / 127.0) - 1
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    data[0] = normalized_image_array
+    pred = model.predict(data)[0]
+    return pred[0]
+
+# Configuración para desactivar la advertencia
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# Encabezado
+st.title('MODELO PARA PERROS Y GATOS')
+
+# Sección de carga de imágenes
+uploaded_file = st.file_uploader("Selecciona una imagen", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # Guardar temporalmente el archivo
+    temp_path = os.path.join(temp_dir, uploaded_file.name)
+    with open(temp_path, "wb") as f:
+        f.write(uploaded_file.read())
+
+    # Mostrar la imagen
+    lena_rgb = io.imread(temp_path) / 255.0
+
+    # Crear una figura de Matplotlib y pasarla a st.pyplot()
+    fig, ax = plt.subplots()
+    ax.imshow(lena_rgb)
+    ax.set_title("Imagen seleccionada")
+    ax.axis('off')
+    st.pyplot(fig)
+
+    # Clasificar la imagen
+    pred = clasificar_imagen(temp_path)
+
+    # Mostrar resultado de la clasificación
+    class_index = np.argmax(pred)
+    class_name = class_names[class_index].strip()
+
+    # Estilos personalizados
+    tipo_melanoma_style = f"font-size: 40px; color: {'red' if class_index == 1 else 'green'}"
+    probabilidad_style = "font-size: 40px;"
+    #0 gato
+    #1 perro
+    # Mostrar resultado de la clasificación con estilos personalizados
+    st.markdown(f'<p style="{tipo_melanoma_style}">Tipo de animal: {class_name[2:]}</p>', unsafe_allow_html=True)
+    st.markdown(f'<p style="{probabilidad_style}">Probabilidad: {pred:.5f}</p>', unsafe_allow_html=True)
+
+    # Eliminar el archivo temporal después de usarlo
+    os.remove(temp_path)
